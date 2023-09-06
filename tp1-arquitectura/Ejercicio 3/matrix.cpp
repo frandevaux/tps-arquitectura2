e596bb0b4d2a91416b979f225b9fc69678d81305
Francisco Devaux <frandevaux2013@gmail.com>
2023-09-06 16:54:25 -0300

    exercise 3 fixed

--- tp1-arquitectura/Ejercicio 3/matrix.cpp
+++ tp1-arquitectura/Ejercicio 3/matrix.cpp
@@ -2,33 +2,41 @@
 
 using namespace std;
 
-const int n = 3000;
+const int n = 5;
 
 // matrixC
 float matrixC[n][n];
 
-void calculateCell(int i, int j, float matrixA[n][n], float matrixB[n][n]) {
-    float  result = 0;
-    for (int k = 0; k < n; k++) {
+void calculateCell(int i, int j, float matrixA[n][n], float matrixB[n][n])
+{
+    float result = 0;
+    for (int k = 0; k < n; k++)
+    {
         result += matrixA[i][k] * matrixB[k][j];
-    } 
+    }
     matrixC[i][j] = result;
 }
 
-void singleThread(float matrixA[n][n], float matrixB[n][n]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void singleThread(float matrixA[n][n], float matrixB[n][n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             calculateCell(i, j, matrixA, matrixB);
         }
     }
-} 
+}
 
-int main() {
+int main()
+{
     // init matrices randomly
     float matrixA[n][n];
     float matrixB[n][n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             matrixA[i][j] = rand() % 100;
             matrixB[i][j] = rand() % 100;
         }
