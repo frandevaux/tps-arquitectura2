a51a111bc179ee1d6204229f1e3bbbcf1f1b61a3
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2023-09-18 21:41:59 -0300

    add stopwatch to matrix

--- tp1-arquitectura/Ejercicio 3/matrix.cpp
+++ tp1-arquitectura/Ejercicio 3/matrix.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <sys/time.h>
 
 using namespace std;
 
-const int n = 300;
+const int n = 1000;
 const int t = 10;
 
 // matrixC
@@ -43,9 +44,27 @@ void calculateRows(int start, int end, const vector<vector<float>> &matrixA, con
     }
 }
 
+void printResult(const vector<vector<float>> &matrix) {
+    // print the corners of the matrix
+    cout << "|" << matrix [0][0] << "..." << matrix[0][n-1] << "|" << endl;
+    cout << "|............|" << endl;
+    cout << "|" << matrix [n-1][0] << "..." << matrix[n-1][n-1] << "|" << endl;
+
+
+    // calculate the total
+    float total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            total += matrix[i][j];
+        }
+    }
+    cout << "La suma de los elementos de la matriz es " <<total << endl;
+}
+
 void multiThread(const vector<vector<float>> &matrixA, const vector<vector<float>> &matrixB)
 {
-    // TODO
     int fraction_number = n / t;
     thread thread_array[t];
 
@@ -60,6 +79,8 @@ void multiThread(const vector<vector<float>> &matrixA, const vector<vector<float
             thread_array[i].join();
         }
     }
+
+    printResult(matrixC);
 }
 
 void singleThread(const vector<vector<float>> &matrixA, const vector<vector<float>> &matrixB)
@@ -71,7 +92,11 @@ void singleThread(const vector<vector<float>> &matrixA, const vector<vector<floa
             calculateCell(i, j, matrixA, matrixB);
         }
     }
+
+    printResult(matrixC);
 }
+
+
 
 int main()
 {
@@ -88,40 +113,29 @@ int main()
         }
     }
 
-    cout << "Matrix A: " << endl;
-
-    cout << " " << endl;
-
+    // cout << "Matrix A: " << endl;
+    // cout << " " << endl;
     // imprimirMatriz(matrixA);
 
-    cout << " " << endl;
-
-    cout << "Matrix B: " << endl;
-
-    cout << " " << endl;
+    // cout << "Matrix B: " << endl;
+    // cout << " " << endl;
     // imprimirMatriz(matrixB);
 
-    cout << " " << endl;
-    // singleThread(matrixA, matrixB);
+    cout << "Tamaño de las matrices: " << n << "x" << n << endl;
+
+    timeval time1, time2, time3;
+    gettimeofday(&time1, NULL);
+
+    cout << "\nCalculando multiplicación de matrices con un hilo" << endl;
+    singleThread(matrixA, matrixB);
+    gettimeofday(&time2, NULL);
+    cout << "Tiempo de ejecución: " << (time2.tv_sec - time1.tv_sec) * 1000000 + (time2.tv_usec - time1.tv_usec) << " microsegundos" << endl;
+
+    cout << "\nCalculando multiplicación de matrices con 10 hilos" << endl;
     multiThread(matrixA, matrixB);
+    gettimeofday(&time3, NULL);
+    cout << "Tiempo de ejecución: " << (time3.tv_sec - time2.tv_sec) * 1000000 + (time3.tv_usec - time2.tv_usec) << " microsegundos" << endl;
 
-    cout << "Matrix C: " << endl;
-
-    // imprimirMatriz(matrixC);
-    cout << " " << endl;
-    cout << matrixC[0][0] << endl;
-    cout << " " << endl;
-
-    // calculate total of matrixC
-    float total = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            total += matrixC[i][j];
-        }
-    }
-
-    cout << total << endl;
+    
     return 0;
 }
