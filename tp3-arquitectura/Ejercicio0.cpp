f978dd9bf51d620abbd338b806c5e59141f4ff59
Francisco Devaux <frandevaux2013@gmail.com>
2023-10-04 15:50:43 -0300

    TP3 init

--- tp3-arquitectura/Ejercicio0.cpp
+++ tp3-arquitectura/Ejercicio0.cpp
@@ -1,3 +1,16 @@
-//
-// Created by francisco on 28/09/23.
-//
+
+#include <iostream>
+#include <unistd.h>
+#include "direccion_IP.cpp"
+using namespace std;
+
+int main (){
+    pid_t proceso_id = getpid();
+    char ip[20];
+    obtener_IP(ip);
+    char hostname[256];
+
+    gethostname(hostname, sizeof(hostname));
+    cout << "Hola Mundo! soy el proceso " << proceso_id << " de <TOTAL_PROCESOS> corriendo en la máquina "<< hostname << " IP= "<< ip << endl;
+    return 0;
+}
