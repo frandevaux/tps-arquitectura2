a3ac2c7e1724476e52a6bbeab5a501e5383956bd
Francisco Devaux <frandevaux2013@gmail.com>
2023-10-20 18:42:24 -0300

    Ex4 concluded

--- tp3-arquitectura/Ejercicio 4/primos.cpp
+++ tp3-arquitectura/Ejercicio 4/primos.cpp
@@ -49,7 +49,7 @@ bool isPrimeMT(int i, vector<int> primosRaizN)
 
         if (i % primosRaizN[j] == 0)
         {
-            // cout << i << " es divisible por " << primosRaizN[j] << endl;
+
             return false;
         }
     }
@@ -59,13 +59,11 @@ bool isPrimeMT(int i, vector<int> primosRaizN)
 vector<int> calculatePrimesMP(int start, int end, vector<int> primosRaizN)
 {
     vector<int> local_vector;
-    cout << "Start: " << start << " End: " << end << endl;
+
     for (int i = start; i < end; i++)
     {
         if (isPrimeMT(i, primosRaizN))
         {
-            // print
-            cout << "Proceso " << MPI::COMM_WORLD.Get_rank() << " encontró primo: " << i << endl;
             local_vector.push_back(i);
         }
     }
@@ -87,25 +85,30 @@ int main()
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int n = 1000;
+    const int n = 10000000;
     const int raizN = ceil(sqrt(n));
     int fraction_number = (n - raizN) / (size - 1);
 
     vector<int> primosTotal;
     vector<int> primosLocal;
-    int local_size = 0;
-    int max_size = 0;
+    int local_primes_number = 0;
+    int total_primes_number = 0;
     int globalCorrect = 0;
     int localCorrect = 0;
 
     if (rank == 0)
     {
         vector<int> primosRaizN = generarPrimos(raizN);
+        // print rank and primosRaizN.size
+        cout << "Proceso " << rank << " calculó " << primosRaizN.size() << " primos" << endl;
+
         // send to all processes primosRaizN
         primosLocal = primosRaizN;
-        primosTotal.resize(primosRaizN.size());
+
         // send size of primosRaizN to all processes
         int primosRaizN_size = primosRaizN.size();
+        local_primes_number = primosRaizN_size;
+
         for (int i = 1; i < size; i++)
         {
             MPI_Send(&primosRaizN_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
@@ -117,8 +120,6 @@ int main()
         {
             MPI_Send(primosRaizN.data(), primosRaizN_size, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
-        // print number of primes
-        cout << "Proceso " << rank << " calculó " << primosRaizN_size << " primos" << endl;
     }
     else
     {
@@ -135,32 +136,49 @@ int main()
         // Recibe los datos en primosRaizN
         MPI_Recv(primosRaizN.data(), primosRaizN_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        cout << "Proceso " << rank << " recibió primosRaizN: ";
-        for (int i = 0; i < primosRaizN.size(); i++)
-        {
-            cout << primosRaizN[i] << " ";
-        }
         // calculate primes
         int start = raizN + (rank - 1) * fraction_number;
         int end = raizN + rank * fraction_number;
         primosLocal = calculatePrimesMP(start, end, primosRaizN);
-        local_size = primosLocal.size();
+        local_primes_number = primosLocal.size();
         // print
-        cout << "Proceso " << rank << " calculó " << local_size << " primos" << endl;
+        cout << "Proceso " << rank << " calculó " << local_primes_number << " primos" << endl;
     }
 
-    // calculate the max size from every primosLocal
-    MPI_Reduce(&local_size, &max_size, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-    // resize primosTotal
-    primosTotal.resize(max_size * (size - 1) + primosTotal.size());
-    // gather all the primosLocal in primosTotal
-    MPI_Gather(&primosLocal, max_size, MPI_INT, &primosTotal, max_size, MPI_INT, 0, MPI_COMM_WORLD);
+    // send local primes number to root
+    MPI_Reduce(&local_primes_number, &total_primes_number, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // print total primes number
 
-    // order primosTotal and print size of primosTotal
+    vector<int> sizes(size);
+    MPI_Gather(&local_primes_number, 1, MPI_INT, sizes.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Calculate the displacement for each process in the receive buffer
+    vector<int> displs(size);
+    displs[0] = 0;
+    for (int i = 1; i < size; i++)
+    {
+        displs[i] = displs[i - 1] + sizes[i - 1];
+    }
+
+    // Calculate the total size of the global vector
+    int total_size = displs[size - 1] + sizes[size - 1];
+
+    // resize the global vector
+    primosTotal.resize(total_size);
+
+    // Gather the local vectors to the root process
+    MPI_Gatherv(primosLocal.data(), local_primes_number, MPI_INT, primosTotal.data(), sizes.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
+    // order the global vector
+    sort(primosTotal.begin(), primosTotal.end());
     if (rank == 0)
     {
-        sort(primosTotal.begin(), primosTotal.end());
-        cout << "Cantidad de primos: " << primosTotal.size() << endl;
+        cout << "Total de primos: " << total_primes_number << endl;
+        // print last 10 primes
+        cout << "Últimos 10 primos: ";
+        for (int i = total_size - 10; i < total_size; i++)
+        {
+            cout << primosTotal[i] << " ";
+        }
     }
 
     if (MPI_SUCCESS != MPI_Finalize())
